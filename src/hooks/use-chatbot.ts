6f5873c5d836026a78887a33e6a2840f8c167b18
anonymoussouto-jpg <anonymoussouto@gmail.com>
2026-08-16 18:24:49 -0300

    Initial commit from Lovable: The Royal Cut structure

---
+++ src/hooks/use-chatbot.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+interface ChatbotState {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
+  toggle: () => void;
+}
+
+export const useChatbot = create<ChatbotState>((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+  toggle: () => set((state) => ({ isOpen: !state.isOpen })),
+}));
